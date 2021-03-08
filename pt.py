e085690248ca82440e0c50c6cf9d38977a8810c4
revoltage34 <43719375+revoltage34@users.noreply.github.com>
2021-03-08 09:52:28 +0700

    v1.2 update

--- pt.py
+++ pt.py
@@ -3,13 +3,18 @@
 # https://github.com/revoltage34/ptanalyzer #
 #############################################
 
-version = "v1.1"
-
+import traceback
 import sys
-        
+from sty import ef, fg, rs
+from colorama import init
+init()
+
+version = "v1.2"
+     
 class Analyzer(object):
     def __init__(self):
         self.state = 0
+        self.nickname = False
         self.stateLoop = [3, 4, 9, 12, 13, 18, 19]
         self.stateText = [
         'jobId=/Lotus/Types/Gameplay/Venus/Jobs/Heists/HeistProfitTakerBountyFour', #Get PT Heist
@@ -64,6 +69,8 @@ class Analyzer(object):
             self.damageType.append(self.dictDT[line.split()[-1]])
         if self.state > 0 and 'jobId=/Lotus/Types/Gameplay/Venus/Jobs/Heists/HeistProfitTakerBountyFour' in line:
             return -1 #Run abort detected
+        if not self.nickname and 'Player name is ' in line:
+            self.nickname = line.split()[-1]
         if self.state in self.stateLoop:
             if not self.stateText[self.state + 1] in line:
                 if self.stateText[self.state] in line:
@@ -79,25 +86,33 @@ class Analyzer(object):
             self.state += 1
         return 0 #Still running
     
+def color(text, col):
+    return col + text + rs.fg
+    
 def ErrorMsg():
-    print("\nAn error might have occured, please screenshot the error and report this along with your EE.log attached.")
+    traceback.print_exc()
+    print(color("\nAn error might have occured, please screenshot this and report this along with your EE.log attached.", fg.li_red))
     input('Press ENTER to exit..')
     sys.exit()
 
 
-print("Profit-Taker Analyzer " + version + " by ReVoltage#3425")
-print("https://github.com/revoltage34/ptanalyzer \n")
+print(fg.cyan + "Profit-Taker Analyzer " + version + " by " + fg.li_cyan + "ReVoltage#3425")
+print(color("https://github.com/revoltage34/ptanalyzer \n", fg.white))
 
 try:
     droppedFile = sys.argv[1]
 except:
-    print("How to use:")
+    print(fg.li_green + "How to use:" + fg.li_yellow)
     print("Drag your EE.log to the .exe file or this terminal then hit ENTER")
     print("Support multiple Profit-Taker run per EE.log\n")
-    fname = input('Or just hit ENTER now to exit..\n')
+    print("Or just hit ENTER now to exit..\n" + fg.rs)
+    fname = input()
     droppedFile = str(fname).replace("\"", "")
 
-text = open(droppedFile, 'r').readlines()
+if droppedFile != "":
+    text = open(droppedFile, 'r', encoding='utf-8', errors='ignore').readlines()
+else:
+    sys.exit()
 
 PT = [] #List of class Analyzer
 RT = [] #Timestamps
@@ -105,7 +120,6 @@ RT_c = [] #Gap between timestamps
 
 found = False
 run = 0
-
 
 PT.append(Analyzer())
 RT.append([])
@@ -148,6 +162,7 @@ try:
             #for t in RT_c[run]:
                 #print(str(t) + '\n')
             found = True
+            nickname = PT[run].nickname
             
             PT.append(Analyzer())
             RT.append([])
@@ -164,70 +179,87 @@ try:
     else:
         for i in range(run):
             shieldDT = 0
-            print("------------------------------------------------------------------------")
-        
-            print("Profit-Taker Run #" + str(i + 1) + " beaten in " + str(RT[i][-1]) + "s\n")
-
-            print("Profit-Taker found in " + RT_c[i][2] + "s")
+            print(fg.white + "------------------------------------------------------------------------" + rs.fg)
             
+            mins = str(int(RT[i][-1]/60))
+            secs = str(int(RT[i][-1]%60))
+            ms = str(int(RT[i][-1]%1*1000))
+            print(fg.cyan + "Profit-Taker Run #" + str(i + 1) + " by " + color(nickname, fg.li_cyan) + fg.cyan + " cleared in " + color(mins + "m " + secs + "s " + ms + "ms\n", fg.li_cyan))
+
+            print(color("From elevator to Profit-Taker took " + RT_c[i][2] + "s\n", fg.li_red))
+            
+            #--PHASE 1--#
+            mins = str(int(RT[i][6]/60))
+            secs = str(int(RT[i][6]%60))
+            if RT[i][6]%60 < 10:
+                secs = "0" + secs
+            print(color("> Phase 1 ", fg.li_green) + color("[" + mins + ":" + secs + "]", fg.li_cyan))
             temp = ""
             for t in RT_c[i][3]:
                 if temp != "":
                     temp += " | "
                 temp += PT[i].damageType[shieldDT] + " " + str(t) + "s"
                 shieldDT += 1
-            print("Shield change: " + temp)
+            print(color(" Shield change: ", fg.white) + fg.li_yellow + temp)
             
             temp = ""
             for t in RT_c[i][4]:
                 if temp != "":
                     temp += " | "
                 temp += str(t) + "s"
-            print("Leg break: " + temp)
+            print(color(" Leg break: ", fg.white) + fg.li_yellow + temp)
             
-            mins = str(int(RT[i][6]/60))
-            secs = str(int(RT[i][6]%60))
-            if RT[i][6]%60 < 10:
+            
+            print(color(" Body destroyed in " + RT_c[i][6] + "s\n", fg.white))
+            
+            #--PHASE 2--#
+            mins = str(int(RT[i][11]/60))
+            secs = str(int(RT[i][11]%60))
+            if RT[i][11]%60 < 10:
                 secs = "0" + secs
-            print("Body destroyed in " + RT_c[i][6] + "s [" + mins + ":" + secs + "]\n")
-            
-            print("4 Pylons destroyed in " + RT_c[i] [8] + "s (with ~7s pylon land delay)")
+            print(color("> Phase 2 ", fg.li_green) + color("[" + mins + ":" + secs + "]", fg.li_cyan))
+            print(color(" 4 Pylons destroyed in " + RT_c[i] [8] + "s", fg.white))
             
             temp = ""
             for t in RT_c[i][9]:
                 if temp != "":
                     temp += " | "
                 temp += str(t) + "s"
-            print("Leg break: " + temp)
+            print(color(" Leg break: ", fg.white) + fg.li_yellow + temp)
             
-            mins = str(int(RT[i][11]/60))
-            secs = str(int(RT[i][11]%60))
-            if RT[i][11]%60 < 10:
+            
+            print(color(" Body destroyed in " + RT_c[i][11] + "s\n", fg.white))
+            
+            #--PHASE 3--#
+            mins = str(int(RT[i][15]/60))
+            secs = str(int(RT[i][15]%60))
+            if RT[i][15]%60 < 10:
                 secs = "0" + secs
-            print("Body destroyed in " + RT_c[i][11] + "s [" + mins + ":" + secs + "]\n")
-            
+            print(color("> Phase 3 ", fg.li_green) + color("[" + mins + ":" + secs + "]", fg.li_cyan))
             temp = ""
             for t in RT_c[i][12]:
                 if temp != "":
                     temp += " | "
                 temp += PT[i].damageType[shieldDT] + " " + str(t) + "s"
                 shieldDT += 1
-            print("Shield change: " + temp)
+            print(color(" Shield change: ", fg.white) + fg.li_yellow + temp)
             
             temp = ""
             for t in RT_c[i][13]:
                 if temp != "":
                     temp += " | "
                 temp += str(t) + "s"
-            print("Leg break: " + temp)
+            print(color(" Leg break: ", fg.white) + fg.li_yellow + temp)
             
-            mins = str(int(RT[i][15]/60))
-            secs = str(int(RT[i][15]%60))
-            if RT[i][15]%60 < 10:
+            print(color(" Body destroyed in " + RT_c[i][15] + "s\n", fg.white))
+            
+            #--PHASE 4--#
+            mins = str(int(RT[i][22]/60))
+            secs = str(int(RT[i][22]%60))
+            if RT[i][22]%60 < 10:
                 secs = "0" + secs
-            print("Body destroyed in " + RT_c[i][15] + "s [" + mins + ":" + secs + "]\n")
-            
-            print("6 Pylons destroyed in " + RT_c[i] [17] + "s (with ~7s pylon land delay)")
+            print(color("> Phase 4 ", fg.li_green) + color("[" + mins + ":" + secs + "]", fg.li_cyan))
+            print(color(" 6 Pylons destroyed in " + RT_c[i] [17] + "s", fg.white))
             
             temp = ""
             for t in RT_c[i][18]:
@@ -235,22 +267,18 @@ try:
                     temp += " | "
                 temp += PT[i].damageType[shieldDT] + " " + str(t) + "s"
                 shieldDT += 1
-            print("Shield change: " + temp)
+            print(color(" Shield change: ", fg.white) + fg.li_yellow + temp)
             
             temp = ""
             for t in RT_c[i][19]:
                 if temp != "":
                     temp += " | "
                 temp += str(t) + "s"
-            print("Leg break: " + temp)
+            print(color(" Leg break: ", fg.white) + fg.li_yellow + temp)
             
-            mins = str(int(RT[i][22]/60))
-            secs = str(int(RT[i][22]%60))
-            if RT[i][22]%60 < 10:
-                secs = "0" + secs
-            print("Body destroyed in " + str(round(float(RT_c[i][22]), 3)) + "s [" + mins + ":" + secs + "]")
+            print(color(" Body destroyed in " + RT_c[i][22] + "s\n", fg.white))
             
-            print("------------------------------------------------------------------------\n\n")
+            print(fg.white + "------------------------------------------------------------------------\n\n" + rs.fg)
         
     input('Press ENTER to exit..')
 except:
